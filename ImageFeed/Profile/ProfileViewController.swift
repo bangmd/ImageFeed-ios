48949e62675f7143f645af324ff1f30d147cef82
Soslan <soslan74d@gmail.com>
2024-03-07 21:51:12 +0300

    some part of sprint_09

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class ProfileViewController: UIViewController{
+    @IBOutlet var iconImageView: UIImageView!
+    @IBOutlet var textLabel: UILabel!
+    @IBOutlet var userNameLabel: UILabel!
+    @IBOutlet var loginNameLabel: UILabel!
+    @IBOutlet var logoutButton: UIButton!
+    
+    @IBAction func logoutButtonDidTap(_ sender: UIButton) {
+    }
+    
+}
