ca124599550d38904f5afb2d22c153f2e91fdd0c
Soslan Dzampaev <>
2024-04-03 23:30:38 +0300

    - Добавлена авторизация через протокол OAuth2

---
+++ ImageFeed/ImagesList/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Soslan Dzampaev on 25.03.2024.
+//
+
+import Foundation
---
+++ ImageFeed/NetworkClient.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+//protocol NetworkRouting {
+//    func fetch(url: URL, handler: @escaping (Result<Data, Error>) -> Void)
+//}
+
+struct NetworkClient {
+    
+    private enum NetworkError: Error {
+        case codeError
+    }
+    
+//    func fetch(urlRequest: URLRequest, handler: @escaping (Result<Data, Error>) -> Void) {
+//        let request = OAuth2Service().makeOAuthTokenRequest(code: code)
+//        
+//        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+//            if let error = error {
+//                handler(.failure(error))
+//                return
+//            }
+//            
+//            if let response = response as? HTTPURLResponse,
+//               response.statusCode < 200 || response.statusCode >= 300 {
+//                handler(.failure(NetworkError.codeError))
+//                return
+//            }
+//            
+//            guard let data = data else { return }
+//            handler(.success(data))
+//        }
+//        
+//        task.resume()
+//    }
+}
